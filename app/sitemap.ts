19b1b8d27382caae1281153bb12c30c4928a2143
MR.BARNS <MRBARNX@GMAIL.COM>
2026-09-17 18:50:44 -0700

    Connect app/sitemap.ts to résumé preview

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,6 +8,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const staticRoutes = [
     '',
     '/projects',
+    '/resume',
     '/projects/barnx-portfolio-cms',
     '/capabilities',
     '/experience',
